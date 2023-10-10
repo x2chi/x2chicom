73469e5e50ac0a283583c3780600a37c5e64ba4b
x2chi <x2chi@x2chi.com>
2023-10-10 10:13:17 +0900

    nextjs
    nextjs 로 변경

---
+++ src/app/loading.js
@@ -0,0 +1,5 @@
+export default function Loading(){
+    return (
+        <div>L O A D I N G . . .</div>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+
+import {use} from "react"
+import Image from 'next/image'
+import {Content, Inter} from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+    return (
+        <main>
+        </main>
+    );
+}
